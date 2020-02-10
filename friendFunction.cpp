687746751136a245b121c88e6e6a82f752950925
Aun Syed Shah <48954645+AunSyedShah@users.noreply.github.com>
2020-02-10 13:31:50 +0500

    Friend Function

---
+++ friendFunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Distance
+{
+    private:
+        int meter;
+    public:
+        Distance(): meter(0) { }
+        //friend function
+        friend int addFive(Distance);
+};
+// friend function definition
+int addFive(Distance d)
+{
+    //accessing private data from non-member function
+    d.meter += 5;
+    return d.meter;
+}
+int main()
+{
+    Distance D;
+    cout<<"Distance: "<< addFive(D);
+    return 0;
+}
