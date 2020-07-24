4adae86ac13177a6165cf4d73c0e9b59fdf34bd8
Syed Aun Muhammad <48954645+AunSyedShah@users.noreply.github.com>
2020-07-24 22:02:03 +0500

    Create classBasics.cpp

---
+++ oop/classBasics.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Date
+{
+private:
+    int day;
+    int month;
+    int year;
+
+public:
+    // default constructor
+    Date()
+    {
+        day = 20;
+        month = 4;
+        year = 1998;
+    }
+
+    // parametrized constructor
+    Date(int d, int m, int y)
+    {
+        day = d;
+        month = m;
+        year = y;
+    }
+
+    void printDate()
+    {
+        cout << "Date : " << day << "/" << month << "/" << year << endl;
+    }
+
+    void setDay(int d)
+    {
+        day = d;
+    }
+    void setMonth(int m)
+    {
+        month = m;
+    }
+
+    void setYear(int y)
+    {
+        year = y;
+    }
+};
+
+int main()
+{
+    Date d(1, 1, 2020);
+    d.printDate();
+    d.setDay(20);
+    d.printDate();
+}
